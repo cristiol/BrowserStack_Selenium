a7b73a350b433d42d2fc2a2ba32e120ddcc65d81
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-11-05 16:22:40 +0200

    Update test_browserstack.py

--- test/test_browserstack.py
+++ test/test_browserstack.py
@@ -5,22 +5,26 @@ from src.signin_page import SignInPage
 import pytest
 
 
+# Define a Signin class that extends SignInPage
 class Signin(SignInPage):
     def __init__(self, driver):
         super().__init__(driver)
 
+    # Method for signing in with good credentials
     def signin_with_good_credentials(self):
+        # Navigate to the sign-in page
         self.driver.get('https://bstackdemo.com/signin')
+        # Perform the sign-in
         self.sign_in()
 
-
+# Fixture for managing the driver session
 @pytest.fixture(scope="function")
 def driver():
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
 
-
+# Test for the checkout process
 def test_checkout(driver):
 
     check = CheckOut(driver)
@@ -28,10 +32,10 @@ def test_checkout(driver):
 
     test = Signin(driver)
     test.signin_with_good_credentials()
-
+    # Add items to the cart and proceed to the checkout process
     homepage.add_to_cart()
     homepage.click_checkout()
-
+    # Fill in billing information
     check.select_firs_name()
     check.select_last_name()
     check.select_address()
@@ -40,15 +44,15 @@ def test_checkout(driver):
     check.click_submit()
     check.get_confirmation_message()
 
-
+# Test for checking copyright
 def test_copyright(driver):
-
+    # Open the main page
     driver.get('https://bstackdemo.com/')
-
+    # Check the copyright notice
     homepage = Homepage(driver)
     homepage.copyright()
 
-
+# test for sign in
 def test_browserstack(driver):
 
     test = Signin(driver)
@@ -57,7 +61,7 @@ def test_browserstack(driver):
     homepage = Homepage(driver)
     homepage.get_username()
 
-
+# Test for checking vendors
 def test_vendors(driver):
 
     test = Signin(driver)
@@ -66,7 +70,7 @@ def test_vendors(driver):
     vendor = Homepage(driver)
     vendor.check_vendor()
 
-
+# Test for adding items to the cart
 def test_add(driver):
 
     test = Signin(driver)
@@ -75,7 +79,7 @@ def test_add(driver):
     cart = Homepage(driver)
     cart.click_cart()
 
-
+# Test for adding items to favorites
 def test_favourites(driver):
 
     test = Signin(driver)
@@ -86,7 +90,7 @@ def test_favourites(driver):
     homepage.click_favourites_btn()
     homepage.check_favourite_page()
 
-
+# Test for removing items from the cart
 def test_remove_from_cart(driver):
 
     test = Signin(driver)
@@ -96,7 +100,7 @@ def test_remove_from_cart(driver):
     homepage.add_to_cart()
     homepage.remove_cart()
 
-
+# Test for selecting quantity
 def test_quantity_selection(driver):
 
     test = Signin(driver)
